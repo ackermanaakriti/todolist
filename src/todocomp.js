778077b80b09ef0e069e3fc6a26ac7faed3213af
ackermanaakriti <bhandariaakriti897@gmail.com>
2022-02-11 18:19:19 +0545

    ok

--- src/todocomp.js
+++ src/todocomp.js
@@ -2,13 +2,10 @@ import React from 'react';
 import { useState } from 'react';
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import { faCirclePlus } from '@fortawesome/free-solid-svg-icons';
-
-
-
  function Todocomp() {
-    const [MYtask,SetMytask] = useState('');
-    const [myStore,SetStore]= useState([]);
-    const Myupdate=(e)=>
+    const [enteredTask,SetMytask] = useState('');
+    const [Task_to_be_stored,SetStore]= useState([]);
+    const TextUpdate=(e)=>
     { 
     SetMytask(e.target.value);
     }
@@ -16,33 +13,26 @@ import { faCirclePlus } from '@fortawesome/free-solid-svg-icons';
     const Addvalue=()=>
     {
         console.log("hello ello");
-  const newTask=[MYtask];
-  SetStore([...myStore,newTask]);
+  const TaskHolder=[enteredTask];
+  SetStore([...Task_to_be_stored,TaskHolder]);
     }
     const Myline=()=>
     {
-        
+
     }
   return (
     <>
     <h1>My todo list</h1>
     <div>
         <label  htmlFor="myinput">what is to be done? </label> <br/>
-        <input className="enter" onChange={Myupdate} id="myinput"type="text" value={MYtask}placeholder="enter your task here"/>
+        <input className="enter" onChange={TextUpdate} id="myinput"type="text" value={enteredTask}placeholder="enter your task here"/>
        <h3> <FontAwesomeIcon icon={faCirclePlus} onClick={Addvalue}/> </h3>
       <div>
-          <p>your todo list</p>
+          <h3>your todo list</h3>
           <div >
-             
-                 
-                         <p>{myStore}</p>
-                         
-                         </div>
-                     
-                 
+             <p>{Task_to_be_stored}</p>
             
-              
-
+         </div>
           </div>
       </div>
     
