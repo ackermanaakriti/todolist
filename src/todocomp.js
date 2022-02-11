f20a53b7e34d1ef22747058b2126a8cb8060974f
ackermanaakriti <bhandariaakriti897@gmail.com>
2022-02-11 20:20:25 +0545

    commit

--- src/todocomp.js
+++ src/todocomp.js
@@ -2,9 +2,10 @@ import React from 'react';
 import { useState } from 'react';
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import { faCirclePlus } from '@fortawesome/free-solid-svg-icons';
+import{faXmarkCircle} from '@fortawesome/free-solid-svg-icons'
  function Todocomp() {
     const [enteredTask,SetMytask] = useState('');
-    const [Task_to_be_stored,SetStore]= useState([]);
+    const [task_stored,SetStore]= useState([]);
     const TextUpdate=(e)=>
     { 
     SetMytask(e.target.value);
@@ -13,25 +14,32 @@ import { faCirclePlus } from '@fortawesome/free-solid-svg-icons';
     const Addvalue=()=>
     {
         console.log("hello ello");
-  const TaskHolder=[enteredTask];
-  SetStore([...Task_to_be_stored,TaskHolder]);
+  SetStore((olditems)=>
+  {
+      return[...olditems,enteredTask]
+  })
+  SetMytask("");
     }
-    const Myline=()=>
-    {
-
-    }
+    
   return (
     <>
     <h1>My todo list</h1>
-    <div>
-        <label  htmlFor="myinput">what is to be done? </label> <br/>
-        <input className="enter" onChange={TextUpdate} id="myinput"type="text" value={enteredTask}placeholder="enter your task here"/>
-       <h3> <FontAwesomeIcon icon={faCirclePlus} onClick={Addvalue}/> </h3>
+    <div className="mainContainer">
+        <label className="labelname"  htmlFor="myinput">What is to be done? </label> <br/>
+       <div className="divinside"> <input className="enter" onChange={TextUpdate} id="myinput"type="text" value={enteredTask}placeholder="enter your task here"/>
+       <h3 className="addButton"> <FontAwesomeIcon icon={faCirclePlus} onClick={Addvalue}/> </h3></div>
       <div>
           <h3>your todo list</h3>
-          <div >
-             <p>{Task_to_be_stored}</p>
+          <div  className="showContainer">
+              {
+              task_stored.map((mytasks)=>{
+             
+                return <div>  <li>{mytasks}</li> <button><FontAwesomeIcon icon={faXmarkCircle}/></button></div>
+              }
+              )
             
+            
+               }
          </div>
           </div>
       </div>
