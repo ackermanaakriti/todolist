27f5a5afd459139232041de28a7b81f8cb4723d1
ackermanaakriti <bhandariaakriti897@gmail.com>
2022-03-02 07:55:02 +0545

    final commit

--- src/todocomp.js
+++ src/todocomp.js
@@ -1,16 +1,18 @@
 import React from 'react';
 import { useState } from 'react';
-import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
-import { faCirclePlus } from '@fortawesome/free-solid-svg-icons';
-import{faXmarkCircle} from '@fortawesome/free-solid-svg-icons'
+
+import Mymain from './main';
  function Todocomp() {
     const [enteredTask,SetMytask] = useState('');
     const [task_stored,SetStore]= useState([]);
+    
+   
     const SetmyValueNull=()=>
     {
-      console.log('hwllo world');
-   SetMytask([]);
-   
+     const mylists= document.getElementById('myfirstli');
+     mylists.remove();
+
+      console.log('hello world');
     }
     const TextUpdate=(e)=>
     { 
@@ -23,24 +25,11 @@ import{faXmarkCircle} from '@fortawesome/free-solid-svg-icons'
       return[...olditems,enteredTask]
   })
   SetMytask(""); }
- 
+     const myfunction={ func1:SetmyValueNull, func2:TextUpdate, func3:SetStore, fun4:SetMytask ,func5:Addvalue}
+     const myvalue={ stored:task_stored, entered:enteredTask}
     return (
     <>
-    <h1>My todo list</h1>
-    <div className="mainContainer">
-        <label className="labelname"  htmlFor="myinput">What is to be done? </label> <br/>
-       <div className="divinside"> <input className="enter" onChange={TextUpdate} id="myinput"type="text" value={enteredTask}placeholder="enter your task here"/>
-       <h3 className="addButton"> <FontAwesomeIcon icon={faCirclePlus} onClick={Addvalue}/> </h3></div>
-      <div>
-          <h3>your todo list</h3>
-          <div  className="showContainer">
-              {
-              task_stored.map((mytasks)=>{
-                return <div className="mylists">  <li>{mytasks}</li> <button onClick={SetmyValueNull}><FontAwesomeIcon icon={faXmarkCircle}/></button></div>
-              }) }'
-         </div>
-         </div>
-      </div>
+    <Mymain func= {myfunction}  value={myvalue}/>
      </>
   )
 }
