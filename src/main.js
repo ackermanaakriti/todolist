4dafd25a20963bc5a0a0592dcbf3f56cb8df2c21
ackermanaakriti <bhandariaakriti897@gmail.com>
2022-04-26 16:57:07 +0545

    first

--- src/main.js
+++ src/main.js
@@ -1,26 +1,28 @@
 import react from "react";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import { faCirclePlus } from '@fortawesome/free-solid-svg-icons';
-import{faXmarkCircle} from '@fortawesome/free-solid-svg-icons'
+import { faXmarkCircle } from '@fortawesome/free-solid-svg-icons'
 
-const Mymain=( props)=>
-{ 
-    return<>
+const Mymain = (props) => {
+  return <>
     <h1>My todo list</h1>
     <div className="mainContainer">
-        <label className="labelname"  htmlFor="myinput">What is to be done? </label> <br/>
-       <div className="divinside"> <input className="enter" onChange={props.func.func2} id="myinput"type="text" value={props.value.entered}placeholder="enter your task here"/>
-       <h3 className="addButton"> <FontAwesomeIcon icon={faCirclePlus} onClick={props.func.func5}/> </h3></div>
+      <label className="labelname" htmlFor="myinput">What is to be done? </label> <br />
+      <div className="divinside"> <input className="enter" onChange={props.func.func2} id="myinput" type="text" value={props.value.entered} placeholder="enter your task here" />
+        <h3 className="addButton"> <FontAwesomeIcon icon={faCirclePlus} onClick={props.func.func5} /> </h3></div>
       <div>
-          <h3>your todo list</h3>
-          <div  className="showContainer">
-              {
-              props.value.stored.map((mytasks,index)=>{
-                return <div key={index} className="mylists">  <li  className="myfirstli">{mytasks}</li> <button onClick={()=>props.func.func1(index)}><FontAwesomeIcon icon={faXmarkCircle} /></button></div>
-              }) }'
-         </div>
-         </div>
+        <h3>your todo list</h3>
+        <div className="showContainer">
+          {
+            props.value.stored.map((mytasks, index) => {
+              return <div  className="mylists">  
+              <li key={index}className="myfirstli">{mytasks}</li> 
+              <button onClick={() => props.func.func1}>
+                <FontAwesomeIcon icon={faXmarkCircle} /></button></div>
+            })}'
+        </div>
       </div>
-      </>
+    </div>
+  </>
 }
 export default Mymain;
--- src/todocomp.js
+++ src/todocomp.js
@@ -2,42 +2,47 @@ import React from 'react';
 import { useState } from 'react';
 
 import Mymain from './main';
- function Todocomp() {
-    const [enteredTask,SetMytask] = useState('');
-    const [task_stored,SetStore]= useState([]);
-    
-   
-    const SetmyValueNull=(id)=>
-    {
-        console.log(id);
-        console.log('hello');
-      const updatedvalue= task_stored.filter((currel, ind)=>
-      {
-        return ind!==id;
-      })
-      SetMytask(updatedvalue);
-
-     };
-     
-    
-    const TextUpdate=(e)=>
-    { 
+function Todocomp() {
+  const [enteredTask, SetMytask] = useState('');
+  const [task_stored, SetStore] = useState([]);
+  const TextUpdate = (e) => {
     SetMytask(e.target.value);
-    }
-    const Addvalue=()=>
-    {  console.log("this is hell");
-  SetStore((olditems)=>
-  {
-      return[...olditems,enteredTask]
-      
-  })
-  SetMytask(""); };
-     const myfunction={ func1:SetmyValueNull, func2:TextUpdate, func3:SetStore, fun4:SetMytask ,func5:Addvalue}
-     const myvalue={ stored:task_stored, entered:enteredTask}
-    return (
+  }
+
+
+
+  const Addvalue = () => {
+    console.log("this is hell");
+    SetStore((olditems) => {
+      return [...olditems, enteredTask]
+
+    })
+    SetMytask("");
+  };
+
+
+
+  const SetmyValueNull = (id) => {
+    console.log(id);
+    console.log('hello');
+    const updatedvalue = task_stored.filter((currel, ind) => {
+      return ind !== id;
+    })
+    SetMytask(updatedvalue);
+
+  };
+
+
+ 
+
+  const myfunction = { func1: SetmyValueNull, func2: TextUpdate, func3: SetStore, fun4: SetMytask, func5: Addvalue }
+  const myvalue = { stored: task_stored, entered: enteredTask }
+
+  
+  return (
     <>
-    <Mymain func= {myfunction}  value={myvalue}/>
-     </>
+      <Mymain func={myfunction} value={myvalue} />
+    </>
   )
 }
 export default Todocomp;
